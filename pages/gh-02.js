6c36a633995cc9a9733c8d5af88bc3f459700942
Your Name <you@example.com>
2020-11-19 14:01:38 +0900

    push github

---
+++ pages/gh-02.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Index = () => {
+    return (
+        <>
+            <div>gh pages 02</div>
+            <Link href="/gh-01">
+                <a>gh-01</a>
+            </Link>
+        </>
+    )
+}
+
+export default Index
