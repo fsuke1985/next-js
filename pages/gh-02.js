1304a33bb61a41907b6662281efc221805367b21
fsuke1985 <fsuke1985@gmail.com>
2021-01-01 11:56:20 +0900

    component

--- pages/gh-02.js
+++ pages/gh-02.js
@@ -1,18 +1,17 @@
 import Link from 'next/link'
+import Layout from 'components/layout';
 
 const Index = ({ data }) => {
     return (
-        <>
+        <Layout home={false}>
             <div>gh pages 02</div>
             <Link href="/gh-01">
                 <a>gh-01</a>
             </Link>
             <p>mod</p>
             { data }
-        </>
+        </Layout>
     )
 }
 
 export default Index
-
-
