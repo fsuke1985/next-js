068a9d4f915f88343d12dbe055c96396323bf9d7
fsuke1985 <fsuke1985@gmail.com>
2021-01-01 14:39:16 +0900

    add episodes

---
+++ components/Molecules/episodes.js
@@ -0,0 +1,5 @@
+export default function Episodes() {
+    return (
+        <p>Episodes</p>
+    );
+}
---
+++ pages/module_episodes.js
@@ -0,0 +1,10 @@
+import Layout from 'components/Layout';
+import Episodes from 'components/Molecules/episodes';
+
+export default () => {
+    return (
+        <Layout>
+        <Episodes />
+        </Layout>
+    )
+}
